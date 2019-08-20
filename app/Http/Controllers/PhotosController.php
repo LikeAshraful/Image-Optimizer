6b17d5f768a409f27533d3a9a49513c3b0c96f5d
maipalash <maipalash35@gmail.com>
2019-08-20 16:46:20 +0600

    dropjone configured

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -6,13 +6,40 @@ use Illuminate\Http\Request;
 
 class PhotosController extends Controller
 {
+    private $photos_path; 
+    public function __construct()
+    {
+        $this->photos_path = public_path('/images');
+    }
     
-    public function store() {
-        $this->validate(request(), [
-            'photo' => 'required|image:jpeg '
-        ]);
+    public function store(Request $request) {
 
-        request()->photo->storeAs('images', 'optimized.jpg');
+        $photos = $request->file('file');
+ 
+        if (!is_array($photos)) {
+            $photos = [$photos];
+        } 
+        if (!is_dir($this->photos_path)) {
+            mkdir($this->photos_path, 0777);
+        }
+
+        for ($i = 0; $i < count($photos); $i++) {
+
+            $photo = $photos[$i];
+            $fileName = $photo->getClientOriginalName();          
+            $save_name = $fileName . str_random(2) . '.' . $photo->getClientOriginalExtension(); 
+            $photo->move($this->photos_path, $save_name);
+ 
+        }
+
+        // return Response::json([
+        //     'message' => 'Image saved Successfully'
+        // ], 200);
+
+        // $this->validate(request(), [
+        //     'photo' => 'required|image:jpeg '
+        // ]);
+        // request()->photo->storeAs('images', 'optimized.jpg');
 
         return response('OK', 201);
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,28 +25,12 @@
                 height: 100vh;
             }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
             .content {
                 text-align: center;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 55px;
             }
 
             .links > a {
@@ -58,11 +42,34 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
+            #my-dropzone .message {
+                font-family: "Segoe UI Light", "Arial", serif;
+                font-weight: 600;
+                color: #0087F7;
+                font-size: 1.5em;
+                letter-spacing: 0.05em;
+            }
+            
+            .dropzone {
+                border: 2px dashed #0087F7;
+                background: white;
+                border-radius: 5px;
+                min-height: 300px;
+                padding: 90px;
+                vertical-align: baseline;
+                background: #ebeff1;
+                margin:90px;
+            }
         </style>
+        <link rel="stylesheet" href="/vendor/dropzone-5.5.0/dist/basic.css">
+
+        <script src="/vendor/jquery.min.js"></script>
+        <script src="/vendor/dropzone-5.5.0/dist/dropzone.js"></script>
+        <script src="/js/dropzone-config.js"></script>
+        
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -78,19 +85,78 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <form action="/photos" enctype="multipart/form-data" method="POST">
+                <div class="title m-b-md">
+                    Online Image Compressor
+                </div>  
+                <!-- <form action="/photos" enctype="multipart/form-data" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
                         <label for="">Photo</label>
                         <input class="form-control" name="photo" type="file" />
                         <button type="submit">Upload</button>
                     </div>
-                </form>
+                </form> -->
+                <div class="drop_area">
+                    <form action="{{ url('/photos')}}" enctype="multipart/form-data" method="POST" class="dropzone" id="my-dropzone">
+                        {{ csrf_field() }}
+                        <div class="dz-message">
+                            <div class="col-xs-8">
+                                <div class="message">
+                                    <p>Drop files here or Click to Upload</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="fallback">
+                            <input type="file" name="file" multiple>
+                        </div>                   
+                    </form>
+                </div>
+
+                {{--Dropzone Preview Template--}}
+            <div id="preview" style="display: none;">
+        
+                <div class="dz-preview dz-file-preview">
+                    <div class="dz-image"><img data-dz-thumbnail /></div>
+        
+                    <div class="dz-details">
+                        <div class="dz-size"><span data-dz-size></span></div>
+                        <div class="dz-filename"><span data-dz-name></span></div>
+                    </div>
+                    <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+                    <div class="dz-error-message"><span data-dz-errormessage></span></div>
+                    <div class="dz-success-mark">        
+                        <svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                            <!-- Generator: Sketch 3.2.1 (9971) - http://www.bohemiancoding.com/sketch -->
+                            <title>Check</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs></defs>
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                                <path d="M23.5,31.8431458 L17.5852419,25.9283877 C16.0248253,24.3679711 13.4910294,24.366835 11.9289322,25.9289322 C10.3700136,27.4878508 10.3665912,30.0234455 11.9283877,31.5852419 L20.4147581,40.0716123 C20.5133999,40.1702541 20.6159315,40.2626649 20.7218615,40.3488435 C22.2835669,41.8725651 24.794234,41.8626202 26.3461564,40.3106978 L43.3106978,23.3461564 C44.8771021,21.7797521 44.8758057,19.2483887 43.3137085,17.6862915 C41.7547899,16.1273729 39.2176035,16.1255422 37.6538436,17.6893022 L23.5,31.8431458 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z" id="Oval-2" stroke-opacity="0.198794158" stroke="#747474" fill-opacity="0.816519475" fill="#FFFFFF" sketch:type="MSShapeGroup"></path>
+                            </g>
+                        </svg>        
+                    </div>
+                    <div class="dz-error-mark">        
+                        <svg width="54px" height="54px" viewBox="0 0 54 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
+                            <!-- Generator: Sketch 3.2.1 (9971) - http://www.bohemiancoding.com/sketch -->
+                            <title>error</title>
+                            <desc>Created with Sketch.</desc>
+                            <defs></defs>
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+                                <g id="Check-+-Oval-2" sketch:type="MSLayerGroup" stroke="#747474" stroke-opacity="0.198794158" fill="#FFFFFF" fill-opacity="0.816519475">
+                                    <path d="M32.6568542,29 L38.3106978,23.3461564 C39.8771021,21.7797521 39.8758057,19.2483887 38.3137085,17.6862915 C36.7547899,16.1273729 34.2176035,16.1255422 32.6538436,17.6893022 L27,23.3431458 L21.3461564,17.6893022 C19.7823965,16.1255422 17.2452101,16.1273729 15.6862915,17.6862915 C14.1241943,19.2483887 14.1228979,21.7797521 15.6893022,23.3461564 L21.3431458,29 L15.6893022,34.6538436 C14.1228979,36.2202479 14.1241943,38.7516113 15.6862915,40.3137085 C17.2452101,41.8726271 19.7823965,41.8744578 21.3461564,40.3106978 L27,34.6568542 L32.6538436,40.3106978 C34.2176035,41.8744578 36.7547899,41.8726271 38.3137085,40.3137085 C39.8758057,38.7516113 39.8771021,36.2202479 38.3106978,34.6538436 L32.6568542,29 Z M27,53 C41.3594035,53 53,41.3594035 53,27 C53,12.6405965 41.3594035,1 27,1 C12.6405965,1 1,12.6405965 1,27 C1,41.3594035 12.6405965,53 27,53 Z" id="Oval-2" sketch:type="MSShapeGroup"></path>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+
+                </div>
             </div>
+            {{--End of Dropzone Preview Template--}}                
+            </div>           
+
         </div>
+
+
     </body>
 </html>
